6f532806c0efea4d6e147dd6da1308447b27025c
dvliem <liem.dovan@axonactive.com>
2022-07-11 18:36:29 +0700

    userplace

--- sagas/index.js
+++ sagas/index.js
@@ -1,5 +1,5 @@
-import {all} from 'redux-saga/effects';
-import {watchFetchProducts, watchFetchNews} from './apiRoute';
+import { all } from 'redux-saga/effects';
+import { watchFetchProducts, watchFetchNews } from './apiRoute';
 // import {watchFetchProducts} from './productSage';
 
 export default function* rootSaga() {
